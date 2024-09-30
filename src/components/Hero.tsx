61d925880b2f50318ebb683d4c6383b3368d00bd
Javed Chauhan <chauhanjaved@gmail.com>
2024-09-30 11:58:20 +0530

    use pdf-lib for convertion

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,6 +8,8 @@ import Dropzone from "./UI/Dropzone";
 import SectionHeader from "./UI/SectionHeader";
 import { useState } from "react";
 import ImageList from "./UI/ImageList";
+import { PDFDocument } from "pdf-lib";
+import Button from "./UI/Button";
 
 const Hero = () => {
   const [filePreviews, setFilePreviews] = useState<
@@ -20,6 +22,36 @@ const Hero = () => {
     );
     setFilePreviews(updatedPreviews);
   };
+  // Convert the JPG files to PDF
+  const handleConvertToPdf = async () => {
+    const pdfDoc = await PDFDocument.create();
+
+    for (const { file } of filePreviews) {
+      const imageBytes = await file.arrayBuffer();
+      const image = await pdfDoc.embedJpg(imageBytes);
+      const page = pdfDoc.addPage([image.width, image.height]);
+
+      page.drawImage(image, {
+        x: 0,
+        y: 0,
+        width: image.width,
+        height: image.height,
+      });
+    }
+
+    const pdfBytes = await pdfDoc.save();
+    downloadPdf(pdfBytes);
+  };
+
+  // Helper function to download the PDF file
+  const downloadPdf = (pdfBytes: Uint8Array) => {
+    const blob = new Blob([pdfBytes], { type: "application/pdf" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "converted.pdf";
+    a.click();
+  };
   return (
     <section
       id={HeaderNavItems.Home}
@@ -29,6 +61,14 @@ const Hero = () => {
         caption="JPG to PDF Converter"
         desc="Convert JPG images to PDF in seconds. Easily adjust orientation and margins."
       />
+
+      <Button
+        disabled={filePreviews.length > 0 ? false : true}
+        className="mt-10"
+        caption="Convert"
+        handleClick={handleConvertToPdf}
+      />
+
       <Dropzone setFilePreviews={setFilePreviews} />
       {filePreviews.length > 0 && (
         <ImageList filePreviews={filePreviews} onRemove={handleRemoveFile} />
--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -8,14 +8,16 @@ interface ButtonProps {
   caption: string;
   handleClick: () => void;
   icon?: IconNames;
+  disabled?: boolean;
 }
 
 export default function Button(props: ButtonProps) {
   return (
     <button
       type="button"
+      disabled={props.disabled}
       onClick={props.handleClick}
-      className={`flex items-center justify-center ${props.className ?? ""} ${montserrat.className} rounded bg-blue-ultramarine px-10 py-3 text-lg font-medium tracking-wide text-white transition-all duration-300 hover:opacity-90 active:opacity-90`}
+      className={`flex items-center justify-center ${props.className ?? ""} ${montserrat.className} rounded bg-blue-ultramarine px-10 py-3 text-lg font-medium tracking-wide text-white transition-all duration-300 hover:opacity-90 active:opacity-90 disabled:cursor-not-allowed disabled:opacity-50`}
     >
       <span>{props.caption}</span>
 
