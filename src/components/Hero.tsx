f28c19756368e28d5ed7c910e6ab448a9e3c2621
Javed Chauhan <chauhanjaved@gmail.com>
2024-10-01 12:19:33 +0530

    File selection updations

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,48 +7,12 @@ import { HeaderNavItems } from "@/data/website-data";
 import Dropzone from "./UI/Dropzone";
 import SectionHeader from "./UI/SectionHeader";
 import { useState } from "react";
-import { PDFDocument } from "pdf-lib";
-import Button from "./UI/Button";
-import HeroIcons, { IconNames } from "./UI/HeroIcons";
 
 const Hero = () => {
   const [filePreviews, setFilePreviews] = useState<
     { file: File; preview: string }[]
   >([]);
-  const [openFileDialog, setOpenFileDialog] = useState<(() => void) | null>(
-    null,
-  );
 
-  // Convert the JPG files to PDF
-  const handleConvertToPdf = async () => {
-    const pdfDoc = await PDFDocument.create();
-
-    for (const { file } of filePreviews) {
-      const imageBytes = await file.arrayBuffer();
-      const image = await pdfDoc.embedJpg(imageBytes);
-      const page = pdfDoc.addPage([image.width, image.height]);
-
-      page.drawImage(image, {
-        x: 0,
-        y: 0,
-        width: image.width,
-        height: image.height,
-      });
-    }
-
-    const pdfBytes = await pdfDoc.save();
-    downloadPdf(pdfBytes);
-  };
-
-  // Helper function to download the PDF file
-  const downloadPdf = (pdfBytes: Uint8Array) => {
-    const blob = new Blob([pdfBytes], { type: "application/pdf" });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "converted.pdf";
-    a.click();
-  };
   return (
     <section
       id={HeaderNavItems.Home}
@@ -58,36 +22,7 @@ const Hero = () => {
         caption="JPG to PDF Converter"
         desc="Convert JPG images to PDF in seconds. Easily adjust orientation and margins."
       />
-
-      <Button
-        disabled={filePreviews.length > 0 ? false : true}
-        className="mt-5"
-        caption="Convert to PDF"
-        handleClick={handleConvertToPdf}
-      />
-      <p
-        className={`${filePreviews.length > 0 ? "visible opacity-100" : "invisible opacity-0"} fixed right-[41px] top-[259px] z-[997] flex h-[30px] w-[30px] items-center justify-center rounded-full border border-black-900 bg-blue-ultramarine text-white`}
-      >
-        {filePreviews.length}
-      </p>
-      <button
-        onClick={() => {
-          if (openFileDialog) openFileDialog(); // Call the open dialog function
-        }}
-        className={`${filePreviews.length > 0 ? "visible opacity-100" : "invisible opacity-0"} fixed right-4 top-[280px] z-[996] flex h-[40px] w-[40px] items-center justify-center rounded bg-blue-ultramarine text-white transition-all duration-300 hover:opacity-90 active:opacity-90`}
-      >
-        <HeroIcons
-          iconName={IconNames.Plus}
-          className="size-6 font-bold"
-          strokeWidth={1.5}
-        />
-      </button>
-
-      <Dropzone
-        filePreviews={filePreviews}
-        setFilePreviews={setFilePreviews}
-        setOpenFileDialog={setOpenFileDialog}
-      />
+      <Dropzone filePreviews={filePreviews} setFilePreviews={setFilePreviews} />
     </section>
   );
 };
--- src/components/UI/Dropzone.tsx
+++ src/components/UI/Dropzone.tsx
@@ -1,20 +1,20 @@
-import React, { useCallback, useEffect } from "react";
+import React, { Fragment, useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 import ImageList from "./ImageList";
 import Button from "./Button";
+import HeroIcons, { IconNames } from "./HeroIcons";
+import { PDFDocument } from "pdf-lib";
 
 interface DropzoneProps {
   filePreviews: { file: File; preview: string }[];
   setFilePreviews: React.Dispatch<
     React.SetStateAction<{ file: File; preview: string }[]>
   >;
-  setOpenFileDialog: (open: () => void) => void;
 }
 
 const Dropzone: React.FC<DropzoneProps> = ({
   filePreviews,
   setFilePreviews,
-  setOpenFileDialog,
 }) => {
   // Handle file drop or selection
   const onDrop = useCallback(
@@ -37,17 +37,41 @@ const Dropzone: React.FC<DropzoneProps> = ({
     multiple: true, // Allow multiple file selection
   });
 
-  // Pass the open function to the parent
-  useEffect(() => {
-    setOpenFileDialog(() => open); // Correctly set the open function
-  }, [open, setOpenFileDialog]);
-
   // Handle removing files in the parent component
   const handleRemoveFile = (fileName: string) => {
     const updatedPreviews = filePreviews.filter(
       (f) => f.file.name !== fileName,
     );
     setFilePreviews(updatedPreviews);
+  };
+  // Convert the JPG files to PDF
+  const handleConvertToPdf = async () => {
+    const pdfDoc = await PDFDocument.create();
+
+    for (const { file } of filePreviews) {
+      const imageBytes = await file.arrayBuffer();
+      const image = await pdfDoc.embedJpg(imageBytes);
+      const page = pdfDoc.addPage([image.width, image.height]);
+
+      page.drawImage(image, {
+        x: 0,
+        y: 0,
+        width: image.width,
+        height: image.height,
+      });
+    }
+
+    const pdfBytes = await pdfDoc.save();
+    downloadPdf(pdfBytes);
+  };
+  // Helper function to download the PDF file
+  const downloadPdf = (pdfBytes: Uint8Array) => {
+    const blob = new Blob([pdfBytes], { type: "application/pdf" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "converted.pdf";
+    a.click();
   };
   return (
     <div className="mt-10 w-full">
@@ -60,7 +84,36 @@ const Dropzone: React.FC<DropzoneProps> = ({
       >
         <input {...getInputProps()} />
         {filePreviews.length > 0 ? (
-          <ImageList filePreviews={filePreviews} onRemove={handleRemoveFile} />
+          <Fragment>
+            <div className="sticky top-[100px] z-[999] ml-auto flex flex-row items-center justify-center p-3">
+              <Button
+                className={`${filePreviews.length > 0 ? "visible" : "invisible"} sticky top-[100px] mr-3`}
+                caption="Convert to PDF"
+                handleClick={handleConvertToPdf}
+              />
+              <div className="relative">
+                <p
+                  className={`${filePreviews.length > 0 ? "visible opacity-100" : "invisible opacity-0"} absolute left-0 top-0 z-[1] flex h-[30px] w-[30px] items-center justify-center rounded-full border border-white bg-blue-ultramarine text-white`}
+                >
+                  {filePreviews.length}
+                </p>
+                <button
+                  onClick={open}
+                  className={`${filePreviews.length > 0 ? "visible opacity-100" : "invisible opacity-0"} z-[0] ml-[15px] mt-[15px] flex h-[40px] w-[40px] items-center justify-center rounded bg-blue-ultramarine text-white transition-all duration-300 hover:opacity-90 active:opacity-90`}
+                >
+                  <HeroIcons
+                    iconName={IconNames.Plus}
+                    className="size-6 font-bold"
+                    strokeWidth={1.5}
+                  />
+                </button>
+              </div>
+            </div>
+            <ImageList
+              filePreviews={filePreviews}
+              onRemove={handleRemoveFile}
+            />
+          </Fragment>
         ) : (
           <div className="">
             <Button caption="Select JPG Images" handleClick={open} />
