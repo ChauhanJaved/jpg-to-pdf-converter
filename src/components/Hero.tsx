e86bb8f11a4ac0eb546df029e7e7186fe7814480
Javed Chauhan <chauhanjaved@gmail.com>
2024-10-06 13:05:01 +0530

    Trying dnd-kit component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,6 +8,27 @@ import Dropzone from "./UI/Dropzone";
 import SectionHeader from "./UI/SectionHeader";
 import { useState } from "react";
 
+//Testing
+
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  DragEndEvent,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { SortableItem } from "./UI/SortableItem";
+
+//testing
+
 const Hero = () => {
   const [fileList, setFileList] = useState<File[]>([]);
   const removeFile = (fileToRemove: File) => {
@@ -19,6 +40,28 @@ const Hero = () => {
       ),
     );
   };
+
+  //testing
+  const [items, setItems] = useState([1, 2, 3]);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  );
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+
+    if (over && active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(Number(active.id)); // Convert id to number
+        const newIndex = items.indexOf(Number(over.id)); // Convert id to number
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  };
+  //testing
 
   return (
     <section
@@ -36,6 +79,20 @@ const Hero = () => {
         setFileList={setFileList}
         onRemoveFile={removeFile}
       />
+
+      {/* tsting */}
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
+      >
+        <SortableContext items={items} strategy={verticalListSortingStrategy}>
+          {items.map((id) => (
+            <SortableItem key={id} id={id} />
+          ))}
+        </SortableContext>
+      </DndContext>
+      {/* tsting */}
     </section>
   );
 };
