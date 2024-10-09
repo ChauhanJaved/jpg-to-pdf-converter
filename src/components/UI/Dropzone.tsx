492177523148ddc5ec8de211b636602b8e9f45f3
Javed Chauhan <chauhanjaved@gmail.com>
2024-10-09 12:36:42 +0530

    Toolbar updates

--- src/components/UI/Dropzone.tsx
+++ src/components/UI/Dropzone.tsx
@@ -2,9 +2,9 @@
 import { useDropzone } from "react-dropzone";
 import { handleConvertToPdf } from "@/lib/pdf-lib";
 import { Button } from "./Button";
-import { ArrowRight, CirclePlus, Settings } from "lucide-react";
 import SortableImageList from "./SortableImageList";
 import { useFileContext } from "@/context/FileContext";
+import { Menubar } from "./menubar";
 
 const Dropzone = () => {
   const { fileList, setFileList } = useFileContext();
@@ -18,7 +18,9 @@ const Dropzone = () => {
       })),
     ]);
   };
-
+  const handleClearList = () => {
+    setFileList([]);
+  };
   // Set up the dropzone, restricting to JPG files only
   const { getRootProps, getInputProps, isDragActive, open } = useDropzone({
     onDrop,
@@ -33,44 +35,30 @@ const Dropzone = () => {
       {/* z-index 996 */}
       <div {...getRootProps()}>
         <input {...getInputProps()} />
-        {/* Item---Fixed */}
-        <div
-          className={`${fileList.length === 0 && "hidden"} fixed bottom-[50px] right-[24px] z-[996]`}
-        >
-          <Button onClick={open} size="icon">
-            <CirclePlus className="" />
+        {/* Drag and drop area */}
+        <Menubar className="mb-3 w-full px-3 py-7">
+          <Button variant="outline" onClick={open}>
+            Add Files
           </Button>
-        </div>
-        {/* Tool bar */}
-        <div
-          className={`mb-2 ${fileList.length === 0 && "hidden"} sticky top-[90px] z-[996] mb-5 flex w-full flex-row justify-end space-x-1`}
-        >
-          <Button size="icon">
-            <Settings />
+          <Button variant="outline" onClick={handleClearList}>
+            Clear List
           </Button>
-          {/* Item---2 */}
           <Button
-            className={`sticky top-[100px] text-lg`}
+            variant="outline"
             onClick={() => handleConvertToPdf(fileList)}
           >
             Convert
-            <ArrowRight className="ml-2" />
           </Button>
-        </div>
-        {/* Drag and drop area */}
+        </Menubar>
 
         <div
-          className={`flex min-h-[150px] w-full flex-col items-center justify-center rounded-lg border-2 border-dashed text-center transition md:min-h-[300px] ${
+          className={`flex min-h-[150px] w-full flex-col items-center justify-center rounded-lg border text-center transition md:min-h-[300px] ${
             isDragActive ? "border-blue-500 bg-blue-50" : "border-gray-300"
           }`}
         >
           <SortableImageList />
           <div className={`${fileList.length > 0 && "hidden"}`}>
-            <Button className="text-lg" onClick={open}>
-              Add Files
-              <CirclePlus className="ml-2" />
-            </Button>
-            <p className="mt-3 font-bold text-black-500">or drop files here</p>
+            <p className="font-bold text-black-500">Drop files here</p>
           </div>
         </div>
       </div>
