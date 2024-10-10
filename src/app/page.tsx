68044e6297e71fedb2ad00b862809b302fefbb69
Javed Chauhan <chauhanjaved@gmail.com>
2024-10-10 12:56:32 +0530

    ScrollTop component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import ScrollTop from "@/components/UI/ScrollTop";
 
 export default function Home() {
   return (
     <main id="main" className="text-lg">
       <Hero />
+      <ScrollTop />
     </main>
   );
 }
