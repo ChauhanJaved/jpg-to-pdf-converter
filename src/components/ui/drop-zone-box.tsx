d1606b777a7de8ba112afee18242f3ca5cc25fe6
Javed Chauhan <chauhanjaved@gmail.com>
2024-10-23 11:52:43 +0530

    Badge for total selected files

--- src/components/ui/drop-zone-box.tsx
+++ src/components/ui/drop-zone-box.tsx
@@ -10,7 +10,7 @@ import { useToast } from "@/hooks/use-toast";
 import { Checkbox } from "./check-box";
 import { useState } from "react";
 import { Label } from "./label";
-import { ScrollArea } from "./scroll-area";
+import { Badge } from "./badge";
 
 interface DropZoneBoxProps {
   isDisabled: boolean;
@@ -147,40 +147,40 @@ export default function DropZoneBox({
     <div {...getRootProps()} className="w-full">
       <input {...getInputProps()} />
       <div className="flex items-center justify-between border-t p-3 text-sm sm:text-base">
-        <div>
-          {"Total Files: "}
-          <span className="font-bold">{fileList.length}</span>
+        <div className="flex items-center justify-center gap-2">
+          <Label>Total Files:</Label>
+          <Badge>{`${fileList.length}`}</Badge>
         </div>
         <div className="flex items-center">
           <Checkbox
-            id="imagepreview"
+            id="preview"
             className="mr-1"
             checked={isPreviewVisible}
             onCheckedChange={togglePreview}
           />
-          <Label htmlFor="imagepreview">Image Preview</Label>
+          <Label htmlFor="preview" className="cursor-pointer">
+            File Preview
+          </Label>
         </div>
       </div>
-      <ScrollArea className="border-t">
-        <div
-          className={`flex max-h-[300px] min-h-[150px] w-full flex-wrap items-center justify-center gap-5 p-5 text-center transition sm:max-h-[600px] sm:min-h-[300px] ${isDragActive && "bg-secondary"}`}
-        >
-          {fileList.length > 0 ? (
-            <SortableImageList
-              disabled={isDisabled}
-              isPreviewVisible={isPreviewVisible}
-            />
-          ) : (
-            <div>
-              <p className="m-auto mt-2 px-3 text-base text-gray-500 sm:w-3/4 sm:text-base md:w-3/5 md:text-lg lg:w-1/2">
-                Click the <strong>Add Files</strong> button or{" "}
-                <strong>Drop</strong> your files here. Adjust your settings as
-                needed, then click <strong>Convert</strong> to begin.
-              </p>
-            </div>
-          )}
-        </div>
-      </ScrollArea>
+      <div
+        className={`flex max-h-[300px] min-h-[150px] w-full flex-wrap items-center justify-center gap-5 border-t p-5 text-center transition sm:max-h-[600px] sm:min-h-[300px] ${isDragActive && "bg-secondary"}`}
+      >
+        {fileList.length > 0 ? (
+          <SortableImageList
+            disabled={isDisabled}
+            isPreviewVisible={isPreviewVisible}
+          />
+        ) : (
+          <div>
+            <p className="m-auto mt-2 px-3 text-base text-gray-500 sm:w-3/4 sm:text-base md:w-3/5 md:text-lg lg:w-1/2">
+              Click the <strong>Add Files</strong> button or{" "}
+              <strong>Drop</strong> your files here. Adjust your settings as
+              needed, then click <strong>Convert</strong> to begin.
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
