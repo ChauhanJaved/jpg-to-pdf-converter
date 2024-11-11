ab070b9660813479fb5bfb3b651e017b60fc82fc
Javed Chauhan <chauhanjaved@gmail.com>
2024-11-11 12:37:41 +0530

    Pricing component update

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,33 +8,31 @@ const Hero = () => {
   return (
     <section
       id={HeaderNavItems.Home}
-      className="container mt-[80px] scroll-m-[80px] xl:max-w-screen-xl"
+      className="container px-5 pt-20 lg:px-10 xl:max-w-screen-xl"
     >
-      <div className="mx-5 sm:mx-7 md:mx-9 lg:mx-11">
-        <HeroWithFileProvider />
-        {/* Product description */}
-        <div className="mt-5 flex flex-row gap-2">
-          {/* Box----------1 */}
-          <div className="pt-1">
-            <ShieldCheck className="h-6 w-6" />
-          </div>
-          {/* Box----------2 */}
-          <div className="flex flex-col text-gray-700 dark:text-gray-300">
-            <p className="text-base font-semibold lg:text-lg">
-              Local, Secure, and Private File Conversion
-            </p>
-            <p className="text-base lg:text-lg">
-              Our JPG to PDF converter prioritizes your file privacy and
-              security by processing everything locally on your device. Unlike
-              other converters that require uploading files to external servers,
-              our tool ensures that your images never leave your device. This
-              no-upload approach guarantees maximum data privacy, making it the
-              ideal solution for users handling sensitive or personal files. All
-              conversions happen directly in your browser, ensuring complete
-              security and giving you peace of mind knowing your files are
-              processed safely and remain in your control.
-            </p>
-          </div>
+      <HeroWithFileProvider />
+      {/* Product description */}
+      <div className="mt-5 flex flex-row gap-2">
+        {/* Box----------1 */}
+        <div className="pt-1">
+          <ShieldCheck className="h-6 w-6" />
+        </div>
+        {/* Box----------2 */}
+        <div className="flex flex-col text-gray-700 dark:text-gray-300">
+          <p className="text-base font-semibold lg:text-lg">
+            Local, Secure, and Private File Conversion
+          </p>
+          <p className="text-base lg:text-lg">
+            Our JPG to PDF converter prioritizes your file privacy and security
+            by processing everything locally on your device. Unlike other
+            converters that require uploading files to external servers, our
+            tool ensures that your images never leave your device. This
+            no-upload approach guarantees maximum data privacy, making it the
+            ideal solution for users handling sensitive or personal files. All
+            conversions happen directly in your browser, ensuring complete
+            security and giving you peace of mind knowing your files are
+            processed safely and remain in your control.
+          </p>
         </div>
       </div>
     </section>
--- src/components/pricing.tsx
+++ src/components/pricing.tsx
@@ -16,54 +16,53 @@ export default function Pricing() {
   );
   return (
     portfolioItem && (
-      <section id={HeaderNavItems.Pricing} className="scroll-m-[80px]">
-        <div className="container mx-auto px-3 py-10 xl:max-w-screen-xl">
-          <div className="mx-5 flex flex-col items-center sm:mx-7 md:mx-9 lg:mx-11">
-            <SectionHeader
-              className="pb-10"
-              element="h2"
-              caption={`License Pricing`}
-              desc={
-                portfolioItem.desc
-                  ? portfolioItem.desc
-                  : "One-Time Purchase with Lifetime License | Free Lifetime Upgrades | 15-Day Money-Back Guarantee | Single License Valid for Both Desktop and Web Apps | Secure Payments via FastSpring"
-              }
-            />
-            <div className="grid max-w-[750px] grid-cols-1 gap-5 text-gray-700 dark:text-gray-300 sm:grid-cols-2 lg:grid-cols-3">
-              {licenseOptions.map((license, index) => (
-                <div key={index} className="flex items-center justify-center">
-                  <Link
-                    target="_blank"
-                    href={license.paymentLink}
-                    className="w-[240px]"
-                  >
-                    <div className="flex flex-row gap-1">
-                      <div className="">
-                        <Button variant={"ghost"} size={"icon"}>
-                          <ShoppingCart />
-                        </Button>
-                      </div>
-                      <div className="flex flex-col items-start justify-center">
-                        <p>{license.licenseType} </p>
-                        <p> {license.price}</p>
-                      </div>
+      <section
+        id={HeaderNavItems.Pricing}
+        className="container scroll-m-20 px-5 pt-10 lg:px-10 lg:pt-20 xl:max-w-screen-xl"
+      >
+        <div className="flex flex-col items-center">
+          <SectionHeader
+            element="h2"
+            caption={`License Pricing`}
+            desc={
+              portfolioItem.desc
+                ? portfolioItem.desc
+                : "One-Time Purchase with Lifetime License | Free Lifetime Upgrades | 15-Day Money-Back Guarantee | Single License Valid for Both Desktop and Web Apps | Secure Payments via FastSpring"
+            }
+          />
+          <div className="mt-10 grid max-w-[750px] grid-cols-1 gap-5 text-gray-700 dark:text-gray-300 sm:grid-cols-2 lg:grid-cols-3">
+            {licenseOptions.map((license, index) => (
+              <div key={index} className="flex items-center justify-center">
+                <Link
+                  target="_blank"
+                  href={license.paymentLink}
+                  className="w-[240px]"
+                >
+                  <div className="flex flex-row gap-1">
+                    <div className="">
+                      <Button variant={"ghost"} size={"icon"}>
+                        <ShoppingCart />
+                      </Button>
                     </div>
-                  </Link>
-                </div>
-              ))}
-            </div>
-            <p className="mt-10 text-gray-700 dark:text-gray-300 lg:text-lg">
-              We partner with Bright Market, LLC, dba{" "}
-              <strong>FastSpring</strong>, located at 801 Garden St., Santa
-              Barbara, CA 93101, USA, for secure and efficient payment
-              processing. Once your order is complete, FastSpring will
-              immediately send a license key to the email address associated
-              with your order. To activate your license, simply enter the key in
-              the app. For your privacy, FastSpring does not share any sensitive
-              payment information, such as credit card details, with us. Your
-              transaction is completely secure.
-            </p>
+                    <div className="flex flex-col items-start justify-center">
+                      <p>{license.licenseType} </p>
+                      <p> {license.price}</p>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))}
           </div>
+          <p className="mt-10 text-gray-700 dark:text-gray-300 lg:text-lg">
+            We partner with Bright Market, LLC, dba <strong>FastSpring</strong>,
+            located at 801 Garden St., Santa Barbara, CA 93101, USA, for secure
+            and efficient payment processing. Once your order is complete,
+            FastSpring will immediately send a license key to the email address
+            associated with your order. To activate your license, simply enter
+            the key in the app. For your privacy, FastSpring does not share any
+            sensitive payment information, such as credit card details, with us.
+            Your transaction is completely secure.
+          </p>
         </div>
       </section>
     )
