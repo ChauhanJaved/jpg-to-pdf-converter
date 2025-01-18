bae6079bf7c42178fd5233524b932d78de6567bb
Javed Chauhan <chauhanjaved@gmail.com>
2025-01-18 13:46:29 +0530

    hero button toolbar update

--- src/components/hero-button-toolbar.tsx
+++ src/components/hero-button-toolbar.tsx
@@ -22,12 +22,13 @@ export default function HeroButtonToolbar({
         disabled={disabled}
         className={`${className} h-11 w-16 p-0 sm:h-10 sm:w-auto sm:px-4 sm:py-2`}
         onClick={handleOnClick}
+        aria-label={caption}
       >
-        {Icon && <Icon className="sm:mr-2 sm:h-6 sm:w-6" />}
+        {Icon && <Icon className="sm:mr-2 sm:h-6 sm:w-6" aria-hidden="true" />}
 
         <span className="hidden text-base sm:block">{caption}</span>
       </Button>
-      <span className="mt-1 text-xs sm:hidden">{caption}</span>
+      <span className="mt-1 text-xs sm:sr-only">{caption}</span>
     </div>
   );
 }
